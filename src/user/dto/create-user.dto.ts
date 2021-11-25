bffa98a40c2a3c124a25b34b958d8ef3bf89ef89
hagaitrg <disahagait@gmail.com>
2021-11-25 16:26:18 +0700

    adding validator and transformer

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,7 +1,23 @@
+import { IsEmail, IsOptional, IsString, MaxLength, MinLength } from "class-validator"
+
 export class CreateUserDto {
+    @IsOptional()
     id?: number
+
+    @IsString()
+    @MaxLength(200)
     name_user: string
-    email: string 
+
+    @IsEmail()
+    email: string
+    
+    @IsString()
+    @MinLength(6)
+    @MaxLength(50)
     username: string
+
+    @IsString()
+    @MinLength(8)
+    @MaxLength(32)
     password: string
 }
